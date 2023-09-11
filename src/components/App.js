98731c5a656a42c97af60bd7c3fac3f2834497f1
LizaAkishina <lizaakishina@gmail.com>
2023-09-11 13:33:14 +0300

    feat: скелет аккордеона

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import Accordion from "./Accordion";
 import Footer from "./Footer";
 
 const App = () => {
-
   return (
     <div className="page__content">
       <Header />
+      <Accordion />
       <Footer />
     </div>
   );
