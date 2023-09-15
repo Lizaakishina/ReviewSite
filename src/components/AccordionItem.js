dbd1939cb7b06de75205e0898a08e6e901903985
LizaAkishina <lizaakishina@gmail.com>
2023-09-15 15:44:47 +0300

    feat: корявое изменение фона от radio button

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,13 +1,30 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import { ReactComponent as ArrowIcon} from '../images/ArrowIcon.svg'
 
 export const AccordionItem = ({faqItem, onClick, isOpen, name}) => {
   const itemRef = useRef(null);
+  const [bthStateGreen, setBthStateGreen] = useState(false);
+  const [bthStateGrey, setBthStateGrey] = useState(false);
+  const [bthStateRed, setBthStateRed] = useState(false);
+
+  const handleClickGreen = () => {
+    setBthStateGreen(bthStateGreen => !bthStateGreen);
+  }
+  const handleClickGrey = () => {
+    setBthStateGrey(bthStateGreen => !bthStateGreen);
+  }
+  const handleClickRed = () => {
+    setBthStateRed(bthStateRed => !bthStateRed);
+  }
+
+  let toggleClassCheckGreen = bthStateGreen ? 'acc__header-green' : ' ';
+  let toggleClassCheckGrey = bthStateGrey ? 'acc__header-grey' : ' ';
+  let toggleClassCheckRed = bthStateRed ? 'acc__header-red' : ' ';
 
   return (
     <li className="acc__item">
       <button
-        className="acc__header"
+        className={`acc__header ${toggleClassCheckGreen} ${toggleClassCheckGrey} ${toggleClassCheckRed}`}
         onClick={() => onClick()}
       >
         {faqItem.title}
@@ -23,15 +40,15 @@ export const AccordionItem = ({faqItem, onClick, isOpen, name}) => {
             <div className="acc__result">
               <div className="acc__checkbox">
                 <div className="acc__choice">
-                  <input className="acc__input" type="radio" id="refact" name={name} ></input>
+                  <input className="acc__input" type="radio" id="refact" name={name} value="red" onClick={handleClickRed}></input>
                   <label className="acc__label" for="refact">Надо исправлять</label>
                 </div>
                 <div className="acc__choice">
-                  <input className="acc__input" type="radio" id="maybe" name={name}></input>
+                  <input className="acc__input" type="radio" id="maybe" name={name} value="grey" onClick={handleClickGrey}></input>
                   <label className="acc__label" for="maybe">Можно лучше</label>
                 </div>
                 <div className="acc__choice">
-                  <input className="acc__input" type="radio" id="cool" name={name}></input>
+                  <input className="acc__input" type="radio" id="cool" name={name} value="green" onClick={handleClickGreen}></input>
                   <label className="acc__label" for="cool">Отлично</label>
                 </div>
               </div>
