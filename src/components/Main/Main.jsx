d0cfe706d409205fccdf77e8361c0591910d3600
LizaAkishina <lizaakishina@gmail.com>
2023-11-07 16:02:11 +0300

    feat: декор страницы профиля

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,17 @@ import { memo } from 'react';
 import Header from '../Header/Header';
 import Profile from '../Profile/Profile';
 import Footer from '../Footer/Footer';
+import './Main.css';
 
 const Main = ({loggedIn}) => {
     return (
       <>
         <Header loggedIn={loggedIn}/>
         <main>
+          <div className="decor1"></div>
+          <div className="decor2"></div>
+          <div className="decor3"></div>
+          <div className="decor4"></div>
           <Profile />
         </main>
         <Footer />
