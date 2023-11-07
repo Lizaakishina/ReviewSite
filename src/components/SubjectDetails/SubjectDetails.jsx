c32dde95a97b0f99d591fe88ce51029fa090a34c
LizaAkishina <lizaakishina@gmail.com>
2023-11-07 17:11:00 +0300

    fix: стилизация страницы заданий

--- src/components/SubjectDetails/SubjectDetails.jsx
+++ src/components/SubjectDetails/SubjectDetails.jsx
@@ -4,7 +4,7 @@ import Task from '../Task/Task';
 import logo from '../../images/logo.svg';
 import './SubjectDetails.css';
 
-const TaskData = [
+const TaskDataModule1 = [
   {
     id: 1,
     title: "Лабораторная работа №1",
@@ -23,6 +23,25 @@ const TaskData = [
   },
 ]
 
+const TaskDataModule2 = [
+  {
+    id: 1,
+    title: "Лабораторная работа №4",
+  },
+  {
+    id: 2,
+    title: "Лабораторная работа №5",
+  },
+  {
+    id: 3,
+    title: "Лабораторная работа №6",
+  },
+  {
+    id: 4,
+    title: "Домашняя работа №2",
+  },
+]
+
 const SubjectDetails = () => {
   return (
     <section className="subjectDetails">
@@ -36,7 +55,18 @@ const SubjectDetails = () => {
       <div className="decor7"></div>
       <div className="decor8"></div>
       <div className="subject__list">
-        {TaskData.map((subject) => (
+        <h2 className="subject__module">Модуль 1</h2>
+        {TaskDataModule1.map((subject) => (
+          <Task
+            key={subject.id}
+            id={subject.id}
+            title={subject.title}
+          />
+        ))}
+      </div>
+      <div className="subject__list">
+        <h2 className="subject__module">Модуль 2</h2>
+        {TaskDataModule2.map((subject) => (
           <Task
             key={subject.id}
             id={subject.id}
