5b078859661d8a8053f647fa94c052d17db46a93
LizaAkishina <lizaakishina@gmail.com>
2023-11-08 12:48:04 +0300

    fix: возможность перехода через всю карточку

--- src/components/Subject/Subject.jsx
+++ src/components/Subject/Subject.jsx
@@ -5,9 +5,9 @@ import './Subject.css';
 function Subject({ id, title, semester }) {
   return (
     <div className="subject">
-      <div className="subject__image"></div>
-      <p className="subject__semester">{semester}</p>
       <Link to={`/subject/` + id}>
+        <div className="subject__image"></div>
+        <p className="subject__semester">{semester}</p>
         <p className="subject__name">{title}</p>
       </Link>
     </div>
