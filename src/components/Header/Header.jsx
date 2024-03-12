3d1838bdaf65069715e540ad2eabae7ac83b9c35
LizaAkishina <lizaakishina@gmail.com>
2024-03-12 15:56:49 +0300

    fix: удаление ненужного состояния header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,19 @@
-import { memo, useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { LoginContext } from '../../context/LoginContext';
+import { memo } from 'react';
+import { NavLink } from 'react-router-dom';
 import chel from '../../images/chel.svg';
 import './Header.css';
 
 const Header = () => {
-  const loggedIn = useContext(LoginContext);
-
   return (
     <header className="header">
-      {loggedIn? <>
-          <div className="header__nav">
-            <div className="header__nav header__nav_type_acс">
-              <NavLink to="/users/me" className="link header__link_type_acc">Аккаунт</NavLink>
-              <div className="header__chelbox">
-                <img src={chel} className="header__chel" alt='человечек'/>
-              </div>
-            </div>
+      <div className="header__nav">
+        <div className="header__nav header__nav_type_acс">
+          <NavLink to="/users/me" className="link header__link_type_acc">Аккаунт</NavLink>
+          <div className="header__chelbox">
+            <img src={chel} className="header__chel" alt='человечек'/>
           </div>
-        </>
-        :<>
-          <div className="header__nav">
-            <Link to="/auth/register" className="link header__link">Регистрация</Link>
-            <Link to="/auth/jwt/login" className="link header__link header__link_type_signin">Войти</Link>
-          </div>
-        </>
-      }
+        </div>
+      </div>
     </header>
   )
 }
